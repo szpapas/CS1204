d2fad527319dda57edb5e90111dac92948766bb9
szpapas <szpapas@qq.com>
2012-06-15 09:18:05 +0800

    local modify

--- app/controllers/desktop_controller.rb
+++ app/controllers/desktop_controller.rb
@@ -508,7 +508,8 @@ class DesktopController < ApplicationController
         system("mkdir -p #{yxpath}") if !File.exists?(yxpath)
         system("cp #{v.path} #{yxpath}/#{pic_name}")
         system("chmod 644 #{yxpath}/#{pic_name}")
-        system("./dady/bin/save_iamge.rb #{yxpath}, #{pic_name}, #{params['tpjd']}, #{params['tpbz']}, #{inspect_id}, #{task_id} ")
+        puts  ("./dady/bin/save_iamge.rb #{yxpath}  #{pic_name} #{params['tpjd']} #{params['tpbz']} #{inspect_id}  #{task_id} ")
+        system("./dady/bin/save_iamge.rb #{yxpath}  #{pic_name} #{params['tpjd']} #{params['tpbz']} #{inspect_id}  #{task_id} ")
       end
       
     end
