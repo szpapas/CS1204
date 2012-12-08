3548f5f4578edf0df39c7746cc1429af5445bba9
szpapas <szpapas@qq.com>
2012-12-08 20:11:25 +0800

    local modify

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -15,6 +15,18 @@ class MapController < ApplicationController
     end  
   end
   
+  def get2dinfo_wx
+    lon,lat=params['lon'],params['lat']
+    lon = '120.0' if lon.nil?
+    lat = '30.0'  if lat.nil?
+    user = User.find_by_sql("select gid, tbbh, dlmc, qsxz, qsdwmc, zldwmc, shape_leng, shape_area from ms_dltb where ST_within( transform(geomFromText('POINT(#{lon} #{lat})',4326),2364), the_geom);")
+    if user.size > 0 
+      @user =  user[0]
+      render :template => "/map/get2dinfo.html.erb"
+    else
+      render :template => "/map/2derror.html.erb"
+    end  
+  end
   
   def getuserinfo
     @users = User.find_by_sql("select id, email, username from users limit 1;")
